0b122be42150d0615e12427cd59442ed256a658e
Balbaal <iqbal.mi20@gmail.com>
2020-10-06 14:36:09 +0800

    add: controller for handle create event

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,10 +1,31 @@
 const eventModel = require("../models/Event");
 
 module.exports = {
-  createEvent: (req, res) => {
-    res.send("create event");
-  },
   getEvent: (req, res) => {
     res.send("get event");
+  },
+  createEvent: async (req, res) => {
+    const { title, location, participant, note } = rqe.body;
+
+    try {
+      const eventCreated = await eventModel.create({
+        title,
+        location,
+        participant,
+        note,
+        picture: `images/${req.file.filename}`,
+      });
+
+      res.status(201).json({
+        status: "success",
+        message: "success create event",
+        data: eventCreated,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "something wrong when trying to create event",
+      });
+    }
   },
 };
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var router = express.Router();
+const { upload } = require("../middlewares/multer");
 
 // Controller
 const apiController = require("../controllers/apiController");
 
-router.post("/event", apiController.createEvent);
+router.post("/event", upload, apiController.createEvent);
 router.get("/event", apiController.getEvent);
 
 module.exports = router;
