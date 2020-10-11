dc2f05568d1de661f7ab1686f82af51d8165545a
Balbaal <iqbal.mi20@gmail.com>
2020-10-11 14:40:13 +0800

    fix: allowed cross origin

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ const apiRouter = require("./routes/api");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
